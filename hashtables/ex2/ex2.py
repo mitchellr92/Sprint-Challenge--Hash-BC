de63f4ff5f85933ef84649f04a4ef6bcf994c48c
Mitchell Robles <mitchellr92@live.com>
2019-11-01 08:55:14 -0700

    ex2 in hastables complete, tests passing

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,9 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve,)
 
 
 class Ticket:
@@ -19,5 +17,23 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+
+    print(length)
+    for t in tickets:
+        print(t.source, t.destination)
+        hash_table_insert(hashtable, t.source, t.destination)
+
+    print(route)
+
+    key = "NONE"
+
+    for i in range(length):
+        current_ticket = hash_table_retrieve(hashtable, key)
+        key = current_ticket
+        # print(key)
+        route[i] = current_ticket
+        # print(route[i])
+
+    print(route)
 
     return route
